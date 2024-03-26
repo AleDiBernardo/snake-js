53c2a27cb60aa42ea2a83190d3c7fb613a1ce72e
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-03-26 17:35:05 +0100

    feat: added the feature: passes through walls

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 const square = document.querySelector(".square");
 
 document.addEventListener("keydown", function (e) {
+  console.log(square.offsetLeft, "left");
+  console.log(square.offsetTop, "top");
+
   if (e.code === "ArrowDown") {
     square.style.top = square.offsetTop + 50 + "px";
   }
@@ -15,5 +18,26 @@ document.addEventListener("keydown", function (e) {
 
   if (e.code === "ArrowLeft") {
     square.style.left = square.offsetLeft - 50 + "px";
+  }
+
+  // se il quadrato esce dal bordo sinistro riappare dal lato opposto
+  if (square.offsetLeft < 0) {
+    square.style.left = window.innerWidth + "px";
+  }
+
+  // se lo square supera la larghezza della schermata (lato destro) riappare dal lato opposto
+  if (square.offsetLeft > window.innerWidth) {
+    square.style.left = "0";
+  }
+
+  // se il quadrato esce dal bordo superiore riappare dal lato opposto
+  if (square.offsetTop < 0) {
+    square.style.top = window.innerHeight + "px";
+
+  }
+
+  // se lo square supera l'altezza della schermata (lato inferiore) riappare dal lato opposto
+  if (square.offsetTop > window.innerHeight) {
+    square.style.top = "0";
   }
 });
